153376613e8f107f058ecf2cf2b05e6ae949b661
Catalina Arroyo <151681207+caadf@users.noreply.github.com>
2024-07-15 15:59:17 +0000

    listo

--- src/store/flux.js
+++ src/store/flux.js
@@ -16,34 +16,34 @@ const getStore = ({ getStore, getActions, setStore }) => {
         },
         actions: {
             getPeople: () => {
-                fetch("https://swapi.py4e.com/api/people/")
+                fetch("https://swapi.dev/api/people")
                     .then(response => response.json())
                     .then(datos => setStore({ people: datos }))
             },
             getPlanets: () => {
-                fetch("https://swapi.py4e.com/api/planets/")
+                fetch("https://swapi.dev/api/planets/")
                     .then(response => response.json())
                     .then(datos => setStore({ planets: datos }))
             },
             getVehicles: () => {
-                fetch("https://swapi.py4e.com/api/vehicles/")
+                fetch("https://swapi.dev/api/vehicles/")
                     .then(response => response.json())
                     .then(datos => setStore({ vehicles: datos }))
             },
             getDetailsPeople: (id) => {
-                let urls = "https://swapi.py4e.com/api/people/" + id
+                let urls = "https://swapi.dev/api/people/" + id
                 fetch(urls)
                 .then(response => response.json())
                 .then(datos => setStore({ detailedpeople: datos }))
             },
             getDetailsPlanets: (id) => {
-                let urls = "https://swapi.py4e.com/api/planets/" + id
+                let urls = "https://swapi.dev/api/planets/" + id
                 fetch(urls)
                 .then(response => response.json())
                 .then(datos => setStore({ detailedplanets: datos }))
             },
             getDetailsVehicles: (id) => {
-                let urls = "https://swapi.py4e.com/api/vehicles/" + id
+                let urls = "https://swapi.dev/api/vehicles/" + id
                 fetch(urls)
                 .then(response => response.json())
                 .then(datos => setStore({ detailedvehicles: datos }))
